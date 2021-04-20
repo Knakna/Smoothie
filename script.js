c393c8bdbfa302974214febec857c519af4c8a75
knakna <kna@tut.by>
2021-04-20 14:28:28 +0300

    add animation

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ $(document).ready(function () {
         $("body").toggleClass('lock');
     })
 });
+
+
+let anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault()
+        let blockID = anchor.getAttribute('href')
+        document.querySelector(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: 'start'
+        })
+    })
+}
